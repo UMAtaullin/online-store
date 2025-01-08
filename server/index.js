6a5f0d96615a7e589fe8471d889361d858c6a2a5
UMAtaullin <ataullinural@gmail.com>
2025-01-08 05:05:58 +0500

    feat: routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const router = require('./routes/index')
+
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+// app.get('/', (req, res) => {
+//   res.status(200).json({message: 'It works!'})
+// })
+app.use('/api', router)
+
 
 const start = async () => {
   try {
