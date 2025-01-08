19b2faa89ae76303b012e12118695500b13dddc0
UMAtaullin <ataullinural@gmail.com>
2025-01-08 06:41:58 +0500

    feat: create ApiError

---
+++ server/error/ApiError.js
@@ -0,0 +1,19 @@
+class ApiError extends Error {
+  constructor(message, status = 500) {
+    super()
+    this.message = message
+    this.status = status
+  }
+
+  static badRequest(message) {
+    return new ApiError(404, message)
+  }
+
+  static internal(message) {
+    return new ApiError(500, message)
+  }
+
+  static forbidden(message) {
+    return new ApiError(403, message)
+  }
+}
