de0b21ef4b62e53c03ea786f97a5168a13118e28
UMAtaullin <ataullinural@gmail.com>
2025-01-09 18:55:09 +0500

    feat: create DeviceStore

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import React, {createContext} from 'react';
 import App from './App.jsx';
 import UserStore from './store/UserStore'
+import DeviceStore from './store/DeviceStore'
 
 export const Context = createContext(null)
 
 createRoot(document.getElementById('root')).render(
   <Context.Provider value={{
-      user: new UserStore()
+      user: new UserStore(),
+      device: new DeviceStore(),
     }}>
   <BrowserRouter>
     <App />
---
+++ client/src/store/DeficeStore.js
@@ -0,0 +1,83 @@
+import { makeAutoObservable } from "mobx";
+
+export default class DeviceStore {
+  constructor() {
+    this._types = [
+      { id: 1, name: 'Телефон' },
+      { id: 2, name: 'Ноутбук' },
+      { id: 3, name: 'Монитор' },
+    ]
+    this._brands = [
+      { id: 1, name: 'Apple' },
+      { id: 2, name: 'Samsung' },
+      { id: 3, name: 'Xiaomi' },
+    ]
+    this.device = [
+      {
+        id: 1,
+        name: 'iPhone 12 Pro Max',
+        price: 100000,
+        rating: 4.5,
+        img: 'https://storage.yandexcloud.net/itech-media/images/products/2022/9/786de063158e11ecb2c03cecef20832b_465c3607159b11ecb2c03cecef20832b.png',
+        typeId: 1,
+        brandId: 1,
+        info: {
+          screenSize: '6.5" OLED',
+          processor: 'A15 Bionic',
+          memory: '64GB',
+        },
+      },
+      {
+        id: 1,
+        name: 'iPhone 13 Pro Max',
+        price: 120000,
+        rating: 4.5,
+        img: 'https://storage.yandexcloud.net/itech-media/images/products/2022/9/786de063158e11ecb2c03cecef20832b_465c3607159b11ecb2c03cecef20832b.png',
+        typeId: 1,
+        brandId: 1,
+        info: {
+          screenSize: '6.5" OLED',
+          processor: 'A15 Bionic',
+          memory: '64GB',
+        },
+      },
+      {
+        id: 1,
+        name: 'iPhone 14 Pro Max',
+        price: 140000,
+        rating: 4.5,
+        img: 'https://storage.yandexcloud.net/itech-media/images/products/2022/9/786de063158e11ecb2c03cecef20832b_465c3607159b11ecb2c03cecef20832b.png',
+        typeId: 1,
+        brandId: 1,
+        info: {
+          screenSize: '6.5" OLED',
+          processor: 'A15 Bionic',
+          memory: '64GB',
+        },
+      },
+
+      
+    ]
+    makeAutoObservable(this)
+  }
+
+  setTypes(types) {
+    this._types = types
+  }
+  setBrands(brands) {
+    this._brands = brands
+  }
+  setDevices(devices) {
+    this._devices = devices
+  }
+
+  get types() {
+    return this._types
+  }
+  get brands() {
+    return this._brands
+  }
+  get devices() {
+    return this._devices
+  }
+}
