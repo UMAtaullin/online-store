58321670054c8e1e2ba018a427f0896c370ab7da
UMAtaullin <ataullinural@gmail.com>
2025-01-10 15:34:04 +0500

    feat: FC Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import AppRouter from './components/AppRouter'
 import NavBar from './components/NavBar'
+import Auth from './pages/Auth'
 
 function App() {
 
   return (
     <>
       <NavBar/>
+      <Auth/>
       <AppRouter />
     </>
   )
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { Container, Form } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const Auth = () => {
+  const location = useLocation()
+  const isLogin = location.pathname === '/login'
+  console.log(location)
+
   return (
-    <div>Auth</div>
+    <Container
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 600 }} className='p-5'>
+        <h2 className='m-auto'>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+        <Form className='d-flex flex-column'>
+          <Form.Control
+            className='mt-3'
+            placeholder='Введите ваш email...'
+          />
+          <Form.Control
+            className='mt-3'
+            placeholder='Введите ваш пароль...'
+            type='password'
+          />
+          <Row className='d-flex justify-content-between mt-3 pl-3 pr-3'>
+            {isLogin ? (
+              <div>
+                Нет аккаунта?{' '}
+                <NavLink to={'/registration'}>Зарегистрируйся!</NavLink>
+              </div>
+            ) : (
+              <div>
+                Есть аккаунт? <NavLink to={'/login'}>Войдите!</NavLink>
+              </div>
+            )}
+            <Button variant={'outline-success'} >
+              {isLogin ? 'Войти' : 'Регистрация'}
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
   )
 }
 
